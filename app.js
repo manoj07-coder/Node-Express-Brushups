36d86d8d84130feee76fa650c0061e7c97a116fe
Manoj <manojgowdacs35@gmail.com>
2023-10-21 23:08:39 +0530

    updated app

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const http = require('http')
+const _ = require('lodash');
 
-const server = http.createServer((req,res) => {
-    if(req.url === '/'){
-        res.end('Welcome to our home page')
-    }
-    if(req.url === '/about'){
-        res.end('here is our short story')
-    }
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `)
-})
-
-server.listen(5000);
+const items = [1, [2, [3, [4, ['name', 6]]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
