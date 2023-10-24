e7ff893668dbe8e26c07a6c364fb2352fb2eb69f
Manoj <manojgowdacs35@gmail.com>
2023-10-24 23:48:18 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const http = require('http')
+const { readFile, writeFile } = require('fs');
+// const { reject } = require('lodash');
+// const { resolve } = require('path');
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end("Home page")
-    }
-    else if (req.url === '/about') {
-        res.end("About page")
-    }else{
-        res.end("Error page")
+
+
+
+const start = async () => {
+    try {
+        const first = await readFilePromise('./content/first.txt','utf8')
+        const second = await readFilePromise('./content/second.txt','utf8')
+        await writeFilePromise('./content/result-mindGrenade-promise',`THIS IS AWESOME: ${first} ${second}`,{flag :'a'})
+        console.log(first, second);
+    } catch (error) {
+        console.log(error)
     }
     
-})
+}
+start()
 
-server.listen(5000, () => {
-    console.log('Server Listening on port : 5000...')
-})
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject ) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err){
+//                 reject(err);
+//             }else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
+
