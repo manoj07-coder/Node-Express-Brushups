d8df9dad44e5cfe5bf346a7fc4afb725c186e0de
Manoj <manojgowdacs35@gmail.com>
2023-11-07 16:36:54 +0530

    ran server

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,17 +1,41 @@
 const http = require('http')
 const { readFileSync } = require('fs')
 
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req,res) => {
    const url = req.url;
+//    Home
    if(url === '/'){
        res.writeHead(200,{'content-type': 'text/html'})
        res.write(homePage)
        res.end()
-   } else if(url === '/about'){
+   }
+//    about  
+   else if(url === '/about'){
     res.writeHead(200,{'content-type': 'text/html'})
        res.write('<h1>About Page</h1>')
+       res.end()
+   }
+//    styles 
+   else if(url === '/styles.css'){
+    res.writeHead(200,{'content-type': 'text/css'})
+       res.write(homeStyles)
+       res.end()
+   }
+//    image 
+   else if(url === '/logo.svg'){
+    res.writeHead(200,{'content-type': 'image/svg+xml'})
+       res.write(homeImage)
+       res.end()
+   }
+//    logic 
+   else if(url === '/browser-app.js'){
+    res.writeHead(200,{'content-type': 'text/javascipt'})
+       res.write(homeLogic)
        res.end()
    }else {
     res.writeHead(404,{'content-type': 'text/html'})
