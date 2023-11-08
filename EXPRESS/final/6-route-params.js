5ac9638b73491551723bfaa19e98dbd55e2dc447
Manoj <manojgowdacs35@gmail.com>
2023-11-08 12:57:51 +0530

    route parameters fixing

---
+++ EXPRESS/final/6-route-params.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express();
+const { products } = require('./data');
+const { reset } = require('nodemon');
+
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product
+        return {id, name, image}
+    })
+
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productId', (req,res)=>{
+
+   const { productId } = req.params;
+
+    const  singleProduct = products.find((product) => product.id === Number(productId))  
+    if(!singleProduct){
+        return res.status(404).send('Product doesnot exist')
+    } 
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productId/reviews/:reviewId', (req,res)=>{
+    console.log(req.params)
+    res.send('Hello World!')
+})
+
+app.get('/api/v1/query', (req, res)=>{
+    const { search, limit } = req.query;
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        // res.status(200).send("no products matched your search")
+        return res.status(200).json({success:true,data:[]})
+    }
+
+    res.status(200).json(sortedProducts)
+    
+
+})
+
+app.listen(5000, () => {
+    console.log('server listening on port 5000')
+})
