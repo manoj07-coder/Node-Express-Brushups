adec20f249c8e77f229cf77e02b7fdb75c9b228e
Manoj <manojgowdacs35@gmail.com>
2023-11-09 19:06:28 +0530

    options lookup

---
+++ EXPRESS/final/9-middleware-options.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const authorize = require('./authorize')
+const morgan = require('morgan')
+
+// app.use([logger,authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('About')
+})
+
+app.get('/api/products', (req,res)=>{
+    res.send('Products')
+})
+
+app.get('/api/items',[logger,authorize], (req,res)=>{
+    console.log(req.user)
+    res.send('Items')
+})
+
+app.listen(5000, ()=>{
+    console.log('server listening on port 5000');
+})
