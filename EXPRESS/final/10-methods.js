f1c14a5f82767acb08a8417068fd5609cfb692ee
Manoj <manojgowdacs35@gmail.com>
2023-11-09 23:31:37 +0530

    methods lookup

--- EXPRESS/final/10-methods.js
+++ EXPRESS/final/10-methods.js
@@ -6,10 +6,6 @@ app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.get('/api/people', (req,res)=>{
-    res.status(200).json({success:true,data:people})
-})
-
 app.post('/login', (req,res)=>{
     const { name } = req.body
     if(name){
@@ -18,12 +14,53 @@ app.post('/login', (req,res)=>{
     res.status(401).send('Please provide credentials')
 })
 
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({success:true,data:people})
+})
+
+
 app.post('/api/people', (req,res)=>{
     const { name } = req.body
     if(!name){
         return res.status(400).json({success:false,msg:'Please provide name value'})
     }
     res.status(200).json({success:true,person:name})
+})
+
+app.post('/api/people/postman', (req,res)=>{
+    const { name } = req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please provide name value'})
+    }
+    res.status(200).json({success:true,data:[...people,name]})
+})
+
+app.put('/api/people/:id', (req,res)=>{
+    const { id } = req.params
+    const { name } = req.body
+
+    const person = people.find((person)=> person.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success:false,msg:`no person with id ${id}`})
+    }
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true,data : newPeople})
+})
+
+app.delete('/api/people/:id', (req,res)=>{
+    const person = people.find((person)=> person.id === Number(req.params.id))
+
+    if(!person){
+        return res.status(404).json({success:false,msg:`no person with id ${req.params.id}`})
+    }
+    const newPeople = people.filter((person)=>person.id !== Number(req.params.id))
+    return res.status(200).json({success:true,data:newPeople})
 })
 
 app.listen(5000, ()=> {
