6c200df533ab7f0564e790e6eca975e3c6f756cc
Manoj <manojgowdacs35@gmail.com>
2023-11-12 13:58:31 +0530

    modified app

--- Task-Manager/starter/app.js
+++ Task-Manager/starter/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
-app.listen(5000, ()=>{
-    console.log('Server listening on port 5000')
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Task Manager app')
+})
+
+app.use('/api/v1/tasks/',tasks)
+
+app.listen(3000, ()=>{
+    console.log('Server listening on port 3000')
 })
