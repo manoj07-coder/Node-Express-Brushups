a1e028c1bd547ba9ab0f323b9b8c9cc8b8541444
Manoj <manojgowdacs35@gmail.com>
2023-11-15 14:02:07 +0530

    controllers modified

--- Task-Manager/starter/controllers/tasks.js
+++ Task-Manager/starter/controllers/tasks.js
@@ -5,8 +5,14 @@ const getAllTasks = (req,res)=>{
 }
 
 const createTask = async (req,res)=>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+        
+    } catch (error) {
+        res.status(500).send({msg:error})
+        
+    }
 }
 
 const getTask = (req,res)=>{
