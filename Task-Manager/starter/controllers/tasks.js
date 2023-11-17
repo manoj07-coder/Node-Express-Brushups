c4f89e4878b8f5a3d45a96bbe6282467851356e9
Manoj <manojgowdacs35@gmail.com>
2023-11-17 17:04:57 +0530

    updating task

--- Task-Manager/starter/controllers/tasks.js
+++ Task-Manager/starter/controllers/tasks.js
@@ -33,9 +33,6 @@ const getTask = async (req,res)=>{
     }
 }
 
-const updateTask = (req,res)=>{
-    res.send('updated task')
-}
 
 const deleteTask = async (req,res)=>{
     try {
@@ -45,11 +42,28 @@ const deleteTask = async (req,res)=>{
             res.status(404).json({msg:`no task with id: ${taskID} to delete...`})
         }
         res.status(200).json({task})
-
+        
     } catch (error) {
         res.status(500).json({msg:error})
     }
 }
+
+const updateTask = async (req,res)=>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true
+        })
+        if(!task){
+            return res.status(404).json({msg:`no task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+}
+
 
 module.exports = {
     getAllTasks,
