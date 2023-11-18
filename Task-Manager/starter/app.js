1d665de55bb28ea04616683c86c02643d722f90e
Manoj <manojgowdacs35@gmail.com>
2023-11-19 00:50:37 +0530

    managing middleware

--- Task-Manager/starter/app.js
+++ Task-Manager/starter/app.js
@@ -4,6 +4,8 @@ const tasks = require('./routes/tasks')
 const connectDB =require('./db/connect')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
 app.use(express.static('./public'))
 app.use(express.json())
@@ -11,6 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1/tasks/',tasks)
+app.use(notFound)
+app.use(errorHandler)
 
 const start = async ()=>{
    try {
