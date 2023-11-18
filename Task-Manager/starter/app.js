79a70cfc18e58f1936ffd230ddd4390ba3dcae0f
Manoj <manojgowdacs35@gmail.com>
2023-11-19 01:01:24 +0530

    setting port to environment variable

--- Task-Manager/starter/app.js
+++ Task-Manager/starter/app.js
@@ -16,11 +16,13 @@ app.use('/api/v1/tasks/',tasks)
 app.use(notFound)
 app.use(errorHandler)
 
+const port = process.env.PORT || 3000
+
 const start = async ()=>{
    try {
        await connectDB(process.env.MONGO_URI)
-       app.listen(3000, ()=>{
-        console.log("server listening on port 3000") 
+       app.listen(port, ()=>{
+        console.log(`Server listening on port ${port}...`) 
     })
    } catch (error) {
     console.log(error)
