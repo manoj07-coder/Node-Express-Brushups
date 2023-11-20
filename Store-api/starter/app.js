0c4d5bd9fcea91ad8b1dfad603f6930dac103c70
Manoj <manojgowdacs35@gmail.com>
2023-11-20 16:14:29 +0530

    setting up basic app requirements

---
+++ Store-api/starter/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect')
+const notFoundMiddleware = require('./middlewares/not-found')
+const errorHandleMiddleware = require('./middlewares/error-handler')
+const productRouter = require('./routes/products')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/',(req,res)=>{
+    res.send(`<h1>Store API</h1><a href='/api/v1/products'>Product route</a>`)
+})
+
+// products routes
+app.use('/api/v1/products',productRouter)
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandleMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
