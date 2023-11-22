ce9a8690ca6c9ddf3614daa7cf340cdc70d11e4d
Manoj <manojgowdacs35@gmail.com>
2023-11-22 12:40:37 +0530

    setting up authorizationError

---
+++ JWT-Basics/starter/errors/unAuthenticated.js
@@ -0,0 +1,11 @@
+const CustomAPIError =  require('../errors/custom-error')
+const {StatusCodes} = require('http-status-codes')
+
+class unAuthenticatedError extends CustomAPIError {
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.UNAUTHORIZED
+    }
+}
+
+module.exports = unAuthenticatedError
