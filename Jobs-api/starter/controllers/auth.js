f46805ab36a3e4a80298e5ceec4fff4dca694bb6
Manoj <manojgowdacs35@gmail.com>
2023-11-23 12:00:23 +0530

    auth and jobs requests

---
+++ Jobs-api/starter/controllers/auth.js
@@ -0,0 +1,18 @@
+const User = require('../models/user')
+const {StatusCodes} = require('http-status-codes')
+
+const register = async (req,res)=>{
+
+    const user = await User.create({...req.body})
+    const token =  user.createJWT();
+    res.status(StatusCodes.CREATED).json({user:{name:user.name} ,token})
+}
+
+const login = async (req,res) => {
+    res.send('Login user')
+}
+
+module.exports = {
+    register,
+    login
+}
