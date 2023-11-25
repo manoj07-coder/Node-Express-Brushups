0e3b3195241dc8d11dbbdf8ee1d2973bf5437c85
Manoj <manojgowdacs35@gmail.com>
2023-11-25 15:21:27 +0530

    handling particular error

--- Jobs-api/starter/middlewares/error-handler.js
+++ Jobs-api/starter/middlewares/error-handler.js
@@ -1,11 +1,36 @@
-const CustomAPIError = require('../errors/custom-error')
+
 const {StatusCodes} = require('http-status-codes')
 
 const errorHandlerMiddleware = async (err,req,res,next)=>{
-    if(err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg:err.message})
+
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:err.message || 'Something went wrong try again later',
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+
+
+    // if(err instanceof CustomAPIError){
+    //     return res.status(err.statusCode).json({msg:err.message})
+    // }
+
+    if(err.name === 'CastError'){
+        customError.msg = `No item found with id : ${err.value}`
+        customError.statusCode = 404
+    }
+
+    if(err.name === 'ValidationError'){
+        customError.msg = Object.values(err.errors).map((item)=>item.message).join(',')
+        customError.statusCode = 400
+    }
+
+
+    if(err.code && err.code === 11000){
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, Please choose another value`
+        customError.statusCode = 400
+    }
+
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    return res.status(customError.statusCode).json({msg:customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
