8ae161cacb9633b9a5ef82e750b29f3dc004e22c
Manoj <manojgowdacs35@gmail.com>
2024-01-25 23:36:37 +0530

    bug fixes

--- Jobs-api/starter/app.js
+++ Jobs-api/starter/app.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimter = require('express-rate-limit')
 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const authenticationMiddleware = require('./middlewares/authentication')
 
@@ -26,6 +29,12 @@ app.use(rateLimter({
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticationMiddleware,jobsRouter)
